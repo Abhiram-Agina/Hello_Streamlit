0a5e9d9ea95ea33896b0e5bb6e95e42f2d7ea221
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2021-12-04 11:18:33 -0800

    Update hellostreamlit.py

--- hellostreamlit.py
+++ hellostreamlit.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1g91afzvnJhX0Hyy090TKPJZ-acxlBDFq
 """
 
-!pip install streamlit
+#!pip install streamlit
 
 import streamlit as st
 
